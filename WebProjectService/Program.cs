c0230bc380404549207abbad20e27abc1bbdf8eb
berkekcay <berkeerenakcay@gmail.com>
2026-03-18 22:57:13 +0300

    fix

--- WebProjectService/Program.cs
+++ WebProjectService/Program.cs
@@ -101,11 +101,10 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
